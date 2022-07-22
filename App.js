4856fa00c3dd65589106b8cb8188f68e76b5ba32
Patrick Rakowicz <lividsubset3@gmail.com>
2022-07-21 20:29:31 -0600

    Main Page - Second Page ++

--- App.js
+++ App.js
@@ -1,19 +1,60 @@
+import * as React from 'react';
 import {StatusBar} from 'expo-status-bar';
-import {StyleSheet, Text, TouchableOpacity, View} from 'react-native';
+import {Button, StyleSheet, Text, TouchableOpacity, View} from 'react-native';
+import {createNativeStackNavigator} from "react-native-screens/native-stack";
+import {NavigationContainer} from "@react-navigation/native";
 
-export default function App() {
+const Stack = createNativeStackNavigator();
+
+function HomeScreen({navigation}) {
     return (
         <View style={styles.container}>
 
-            <Text>The start of LPS (Local Poker System)</Text>
-            <StatusBar style="auto"/>
+            <Text> Home Screen</Text>
 
             <TouchableOpacity
-                onPoress={() => alert('Hello World')} style={styles.button}>
-                <Text style={styles.buttonTest}>New Game</Text>
+                onPress={() => navigation.navigate('Players')} style={styles.button}>
+                <Text style={styles.buttonText}>Players</Text>
             </TouchableOpacity>
 
         </View>
+    );
+}
+
+function PlayerScreen({navigation}) {
+    return (
+        <View style={styles.container}>
+            <View style={styles.container}>
+
+                <Text>The start of LPS (Local Poker System)</Text>
+                <StatusBar style="auto"/>
+
+                <TouchableOpacity
+                    onPress={() => alert('Hello World')} style={styles.button}>
+                    <Text style={styles.buttonText}>New Game</Text>
+                </TouchableOpacity>
+
+                <Button
+                    onPress={() => alert("Learning More")}
+                    title="Learn More"
+                    color="#841584"
+                    accessibilityLabel="Learn more about this purple button"
+                />
+
+            </View>
+        </View>
+    );
+}
+
+export default function App() {
+    return (
+        <NavigationContainer>
+            <Stack.Navigator>
+                <Stack.Screen name={"Home"} component={HomeScreen}/>
+                <Stack.Screen name={"Players"} component={PlayerScreen}/>
+            </Stack.Navigator>
+
+        </NavigationContainer>
     );
 }
 
@@ -29,7 +70,7 @@ const styles = StyleSheet.create({
         padding: 20,
         borderRadius: 5,
     },
-    buttonTest: {
+    buttonText: {
         fontSize: 20,
         color: "white"
     }
